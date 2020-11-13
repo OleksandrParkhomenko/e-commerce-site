e71665718066cdf1c1c864a7967be891f5541f8f
OleksandrParkhomenko <oleksandr.parhomenko@yahoo.com>
2020-11-13 16:35:35 +0200

    Add logout

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('<int:id>/', shop_views.detail, name='detail'),
     path('checkout/', shop_views.checkout, name='checkout'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
