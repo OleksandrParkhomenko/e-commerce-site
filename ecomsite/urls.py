79495d9dfbf74a4f9dd920adecddae21a3b12da7
OleksandrParkhomenko <oleksandr.parhomenko@yahoo.com>
2020-11-13 16:54:56 +0200

    Add registration page

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('checkout/', shop_views.checkout, name='checkout'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', users_views.register, name='register'),
 ]
